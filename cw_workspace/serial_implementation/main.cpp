7fa2285c19d167b14e7eccf2f7b264d470e6e985
Diego del Castillo del Campo <dd519@spitfire.ae.ic.ac.uk>
2023-03-21 16:27:44 +0000

    command line arguments work

--- cw_workspace/serial_implementation/main.cpp
+++ cw_workspace/serial_implementation/main.cpp
@@ -6,8 +6,13 @@ using namespace std;
 #include <iomanip>
 #include <fstream>
 
+#include <boost/program_options.hpp>
+#include <boost/timer/timer.hpp>
+#include <omp.h>
+
 #include "ShallowWater.h"
 
+namespace po = boost::program_options;
 
 int main(int argc, char **argv)
 {
@@ -19,11 +24,42 @@ int main(int argc, char **argv)
     --Ny arg    //Number of grid points in y
     --ic arg    //Index of the initial condition to use (1-4)
     */
-    double dt = 0.1;
-    double T = 20;
-    int Nx = 100;
-    int Ny = 100;
-    int ic = 4;
+    //double dt = 0.1;
+    //double T = 20;
+    //int Nx = 100;
+    //int Ny = 100;
+    //int ic = 4;
+    
+        // Boost program options
+    po::options_description opts("Allowed options");
+    opts.add_options()
+        ("help", "Prints list of options")
+        ("dt", po::value<double>()->default_value(0.1), "Time step")
+        ("T", po::value<double>()->default_value(5.0), "Total simulation time")
+        ("Nx", po::value<int>()->default_value(100), "Number of grid points in x")
+        ("Ny", po::value<int>()->default_value(100), "Number of grid points in y")
+        ("ic", po::value<int>()->default_value(4), "Initial conditions");
+        
+
+    // Default execution:
+    //     ./main --dt 0.1 --T 5 --Nx 100 --Ny 100 --ic 4
+
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, opts), vm);
+    po::notify(vm);
+
+    if (vm.count("help")) {
+        std::cout << opts << "\n";
+        return 1;
+    }
+
+    // Loading parameters into memory
+    double dt     = vm["dt"].as<double>();
+    double T     = vm["T"].as<double>();
+    int Nx     = vm["Nx"].as<int>();
+    int Ny     = vm["Ny"].as<int>();
+    int ic     = vm["ic"].as<int>();
+    
     
     //Instantiate class and parameters via constructor
     int whichIntegrationMethod = 1; // 1: For loop implementation,  2: BLAS implementation
@@ -53,4 +89,6 @@ int main(int argc, char **argv)
     }
     vOut.close();
     cout << "DONE" << endl;
+    
+    return 0;
 }
